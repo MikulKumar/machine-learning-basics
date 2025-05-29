65b9ad883a99420f3257d678eb072bdd5c231560
Mikul Kumar <k.mikul@yahoo.com>
2025-05-28 23:05:48 -0700

    Restore files from earlier commit

---
+++ projects/Fake_news_detector/things_needed.py
@@ -0,0 +1,7 @@
+'''
+Skills: Text preprocessing, vectorization (TF-IDF), logistic regression/SVM
+
+Why it's good: Combines ML + NLP, highly relevant
+
+Bonus: Try using a small dataset from Kaggle and apply your own preprocessing
+'''

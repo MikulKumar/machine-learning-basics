753b448568ed9930cb00624010b82688c2c6a4bf
Mikul Kumar <k.mikul@yahoo.com>
2025-05-28 22:55:57 -0700

    add files again

--- NLP/regular_expression.py
+++
@@ -1,8 +0,0 @@
-import re 
-
-chat1 = "codebasics: you ask alot of questions , 1234568912, abc@xyz.com, 1111111111"
-chat2 = "codebasics: here it is: (123)-567-8912, abc@xyz.com"
-chat3 = "codebasics: yes, phone: 1234568912 email: abc@xyz.com"
-pattern = '\(\d{3}\)-\d{3}-\d{4}'
-matches = re.findall(pattern, chat2)
-print(matches)
--- SVM/SVM.py
+++
@@ -1,6 +0,0 @@
-'''
-when we have 2 categories, but no obvious linear classifier that seperates them in a nice way
-
-Support Vector Machines work by moving the data into a relatively high dimensional space
-
-and finding a relatively high dimensional Support Vector Classifier that can effectively classify the observations
